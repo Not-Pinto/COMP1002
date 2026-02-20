62f97ffa2491fe7b25d572091bfba5e63f57ada8
Liam Kaehler <liamkaehler@gmail.com>
2026-02-20 23:48:34 +0800

    week 2 mostly complete just need to add paragraph

---
+++ week2/DSAsorts.py
@@ -0,0 +1,58 @@
+#
+# Data Structures and Algorithms COMP1002
+#
+# Python file to hold all sorting methods
+#
+
+def bubbleSort(A):
+    for i in range(0, len(A)-1 -1):
+        for j in range(0, len(A)-1-i-1):
+            if A[j] > A[j+1]:
+                temp = A[j]
+                A[j] = A[j+1]
+                A[j+1] = temp
+
+def insertionSort(A):
+    for i in range(1, len(A)-1):
+        j = i
+        while (j>0) and (A[j-1] > A[j]):
+            temp = A[j]
+            A[j] = A[j-1]
+            A[j-j] = temp
+            j = j - 1
+    
+
+def selectionSort(A):
+    for i in range(0, len(A)-1):
+        minIdx = i 
+        for j in range(i+1, len(A)-1):
+            if A[j] < A[minIdx]:
+                minIdx = j
+        temp = A[minIdx]
+        A[minIdx] = A[i]
+        A[i] = temp
+
+
+def mergeSort(A):
+    """ mergeSort - front-end for kick-starting the recursive algorithm
+    """
+    ...
+
+def mergeSortRecurse(A, leftIdx, rightIdx):
+    ...
+
+def merge(A, leftIdx, midIdx, rightIdx):
+    ...
+
+def quickSort(A):
+    """ quickSort - front-end for kick-starting the recursive algorithm
+    """
+    ...
+
+def quickSortRecurse(A, leftIdx, rightIdx):
+    ...
+
+def doPartitioning(A, leftIdx, rightIdx, pivotIdx):
+    ...
+
+
