a0c5d4fa4fd98813106b791ad0d82e5238313b9f
Liam Kaehler <liamkaehler@gmail.com>
2026-02-20 22:30:00 +0800

    finsihed activity 2

--- week1/activity2.py
+++ week1/activity2.py
@@ -1,12 +1,27 @@
 import numpy as np 
 
-dates = np.zeros(365)
-temp = np.zeros(365)
+date = np.empty(365, dtype = "U10")
+temp = np.zeros(365, dtype = int)
 
-with open("temperatures_365_days.csv", "r") as data:
+with open("COMP1002/week1/temperatures_365_days.csv", "r") as data:
     next(data)
 
-    for line in data:
-        temp[line] = int(line.strip().split()[1])
+    for i, line in enumerate(data):
+        date[i] = str(line.strip().split(",")[0])
+        temp[i] = int(line.strip().split(",")[1])
 
-print(temp)
+maxT = temp[0]
+maxD = date[0]
+minT = temp[0]
+minD = date[0]
+
+for i in range (1, 365):
+    if minT > temp[i]:
+        minT = temp[i]
+        minD = date[i]
+    if maxT < temp[i]:
+        maxT = temp[i]
+        maxD = date[i]
+
+print(maxD + " had the hottest day of " + str(maxT) + "C:")
+print(minD + " had the hottest day of " + str(minT) + "C:")
