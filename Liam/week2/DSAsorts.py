1b385cbc3f98c0081846ee65394885481ee71fcf
Not-Pinto <liamkaehler@gmail.com>
2026-02-27 14:39:23 +0800

    h

--- Liam/week2/DSAsorts.py
+++ Liam/week2/DSAsorts.py
@@ -5,12 +5,18 @@
 #
 
 def bubbleSort(A):
-    for i in range(0, len(A)-1 -1):
-        for j in range(0, len(A)-1-i-1):
+
+    i = 0
+    swapped = True
+    while i < len(A) - 1 and swapped:
+        swapped = False
+        for j in range(0, len(A) - 1 - i):
             if A[j] > A[j+1]:
                 temp = A[j]
                 A[j] = A[j+1]
                 A[j+1] = temp
+                swapped = True
+            i += 1
 
 def insertionSort(A):
     for i in range(1, len(A)-1):
@@ -23,9 +29,9 @@ def insertionSort(A):
     
 
 def selectionSort(A):
-    for i in range(0, len(A)-1):
-        minIdx = i 
-        for j in range(i+1, len(A)-1):
+    for i in range(0, len(A) - 1):
+        minIdx = i
+        for j in range(i + 1, len(A)):
             if A[j] < A[minIdx]:
                 minIdx = j
         temp = A[minIdx]
