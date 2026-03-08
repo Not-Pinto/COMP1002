86853e7dd5dc3d2a204d5388bcdce2dfade6342c
Not-Pinto <liamkaehler@gmail.com>
2026-03-08 18:25:50 +0800

    working on week3

--- Liam/week3/FacAndFib.py
+++ Liam/week3/FacAndFib.py
@@ -1,3 +1,42 @@
+def factorialIterative(n):
+    nFactorial = 1
+    for i in range(n, 1, -1):
+        nFactorial = nFactorial * i
+    return nFactorial
+
+def factorialRecursive(n):
+    if n == 0:
+        return 1
+    else: 
+        return n * factorialRecursive(n-1)
+
+def fibonnaciIterative(n):
+    fibVal = 0 
+    currVal = 1
+    lastVal = 0
+
+    if n == 0: 
+        fibVal = 0 
+    elif n == 1: 
+        fibVal = 1
+    else: 
+        for i in range (2, n+1):
+            fibval = currVal + lastVal
+            lastVal = currVal
+            currVal = fibval
+    return fibVal 
+
+def fibonnaciRecursive(n):
+    fibVal = 0
+
+    if n == 0: 
+        fibVal = 0 
+    elif n == 1: 
+        fibVal = 1
+    else: 
+        fibVal = fibonnaciRecursive(n-1) + fibonnaciRecursive (n-2)
+    return fibVal
+
 facOrFib = None
 while facOrFib is None:
     try: 
@@ -22,6 +61,25 @@ while IteOrRec is None:
             print("Please only enter an int, 1 or 2, Error: OutOfRange")
             IteOrRec = None
     
+n = None 
+while n is None:
+    try: 
+        print("What n vaule would you like?")
+        n = int(input("Please eneter a positive int: "))
+    except ValueError as err:
+        print("Please only enter a posive int, Error:", err)
+    else:
+        if 0 > n:
+            print("Please only enter a posive int, Error: OutOfRange")
+            n = None  
 
-        
-
+if facOrFib == 1: 
+    if IteOrRec == 1: 
+        factorialIterative(n)
+    else:
+        factorialRecursive(n)
+else: 
+    if IteOrRec == 1: 
+        fibonnaciIterative(n)
+    else:
+        fibonnaciRecursive(n)
